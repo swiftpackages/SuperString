9c134647d2355439454c37f352d0b34f9e7b458e
marzvrover <marzily@protonmail.com>
2020-12-29 22:41:50 -0500

    init

--- README.md
+++
@@ -1,3 +0,0 @@
-# SuperString
-
-SuperString provides useful string extensions such as `kebabcased`.
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import SuperStringTests
-
-var tests = [XCTestCaseEntry]()
-tests += SuperStringTests.allTests()
-XCTMain(tests)
