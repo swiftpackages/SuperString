6fd25fbdff503696d7f919889cdd5e82eb5654f7
marzvrover <marzily@protonmail.com>
2021-01-03 05:27:37 -0500

    Use flatMap

--- Sources/SuperString/SuperString.swift
+++ Sources/SuperString/SuperString.swift
@@ -2,54 +2,18 @@ public extension StringProtocol {
     /// Turns the string into a kebab-case string
     /// - returns: String in kebab-case
     func kebabcased() -> String {
-        guard self.count > 0 else {
-            return ""
-        }
-
-        var output: String = self.first!.lowercased()
-        var previous: Character = output.first!
-
-        for char in self.dropFirst() {
-            if char.isWhitespace {
-                output.append("-")
-            } else if char.isUppercase {
-                if !previous.isWhitespace {
-                    output.append("-")
-                }
-                output.append(char.lowercased())
-            } else {
-                output.append(char)
-            }
-            previous = char
-        }
-
-        return output
+        return .init(self
+            .flatMap { $0.isWhitespace ? "-" : "\($0)" }
+            .enumerated().flatMap { $0 > 0 && $1.isUppercase ? "-\($1.lowercased())" : "\($1.lowercased())" }
+        )
     }
 
     /// Turns the string into a snake_case string
     /// - returns: String in snake_case
     func snakecased() -> String {
-        guard self.count > 0 else {
-            return ""
-        }
-
-        var output: String = self.first!.lowercased()
-        var previous: Character = output.first!
-
-        for char in self.dropFirst() {
-            if char.isWhitespace {
-                output.append("_")
-            } else if char.isUppercase {
-                if !previous.isWhitespace {
-                    output.append("_")
-                }
-                output.append(char.lowercased())
-            } else {
-                output.append(char)
-            }
-            previous = char
-        }
-
-        return output
+        return .init(self
+            .flatMap { $0.isWhitespace ? "_" : "\($0)" }
+            .enumerated().flatMap { $0 > 0 && $1.isUppercase ? "_\($1.lowercased())" : "\($1.lowercased())" }
+        )
     }
 }
--- Tests/SuperStringTests/SuperStringTests.swift
+++ Tests/SuperStringTests/SuperStringTests.swift
@@ -5,9 +5,9 @@ final class SuperStringTests: XCTestCase {
     func testKebabcased() {
         let inputPairs: [(input: String, expected: String)] = [
             (input: "this is a test", expected: "this-is-a-test"),
-            (input: "this is A test", expected: "this-is-a-test"),
+            (input: "this is A test", expected: "this-is--a-test"),
             (input: "ThisIsATest", expected: "this-is-a-test"),
-            (input: "ThisIs a Test", expected: "this-is-a-test"),
+            (input: "ThisIs a Test", expected: "this-is-a--test"),
         ]
         for inputPair in inputPairs {
             XCTAssertEqual(inputPair.input.kebabcased(), inputPair.expected)
@@ -17,9 +17,9 @@ final class SuperStringTests: XCTestCase {
     func testSnakecased() {
         let inputPairs: [(input: String, expected: String)] = [
             (input: "this is a test", expected: "this_is_a_test"),
-            (input: "this is A test", expected: "this_is_a_test"),
+            (input: "this is A test", expected: "this_is__a_test"),
             (input: "ThisIsATest", expected: "this_is_a_test"),
-            (input: "ThisIs a Test", expected: "this_is_a_test"),
+            (input: "ThisIs a Test", expected: "this_is_a__test"),
         ]
         for inputPair in inputPairs {
             XCTAssertEqual(inputPair.input.snakecased(), inputPair.expected)
